93340736cde1dd5d5b44066c2149249e0760ddcb
omursnck <omrsnr12@icloud.com>
2023-05-02 11:03:04 +0300

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Ömür Şenocak on 2.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
